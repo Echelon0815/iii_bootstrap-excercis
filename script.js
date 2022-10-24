0c4f006da2b889f8a8f90f8dd86871158d5184a3
Echelon0815 at iii <jack.w886@gmail.com>
2022-10-24 20:44:29 +0800

    JS忘了...

---
+++ script.js
@@ -0,0 +1,12 @@
+
+let hiddenMsg = document.querySelector("#hidden-Msg");
+let toggleBtn = document.querySelector(".email-form .reusable-button"); 
+let mediaQ = window.matchMedia("min-width:768px");
+
+if (mediaQ.matches) {
+    hiddenMsg.classList = "d-md-none";
+    toggleBtn.set
+
+
+}else
+
